ed6df882a30d4e75a3516a84366c789bdc1faeda
Georgi Georgiev <ggeorgiev@influxtechnology.com>
2024-12-06 15:14:30 +0200

    rexgen.conf

--- examples/get_rxd_from_rexgen.sh
+++ examples/get_rxd_from_rexgen.sh
@@ -8,7 +8,7 @@ COUNT=$("$PWD"rexgen SD log count | tr -dc '0-9')
 echo "Found " $COUNT " rxd files "
 COUNT=$((COUNT-1))
 
-rm $RXD_DIR*.rxd
+rm -f $RXD_DIR*.rxd
 
 for i in $(seq 0 "$COUNT"); do
         FILE=$("$PWD"rexgen SD file "$i")
