013516b6b04f0a7ad4d7b00cdb82d6cb2a391f8d
Nikolay Lyakov <97745007+nlyakov-influx@users.noreply.github.com>
2025-03-07 15:50:11 +0200

    changes to accept configuration as parameters
    Now the cycle is endless, continuously gets first file in folder, and repeat the process to upload to AWS. 
    
    AWS credentials now is configured via parameters in command line

--- examples/send_rxd_to_s3.sh
+++ examples/send_rxd_to_s3.sh
@@ -1,25 +1,53 @@
 #!/bin/sh
 
-PWD="/home/root/rexusb/"
-
-RXD_DIR=$(cat "$PWD"etc/rexgen.conf | grep 'RXD_FOLDER' | cut -d '=' -f2)
-
-ACCESS_KEY=$("$PWD"/examples/read_aws_conf.sh ACCESS_KEY)
-SECRET_KEY=$("$PWD"/examples/read_aws_conf.sh SECRET_KEY)
-BUCKET=$("$PWD"/examples/read_aws_conf.sh BUCKET)
-
-if [[ $ACCESS_KEY == "" ]] || [[ $SECRET_KEY == "" ]] || [[ $BUCKET == "" ]]; then
-        echo "Fill your Amazon S3 server settings in" "$PWD"etc/aws.conf
-        exit
+# Check if enough arguments are passed
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <WATCH_DIR> <AWS_ACCESS_KEY> <AWS_SECRET_KEY> <BUCKET_NAME> <SERIAL>"
+    exit 1
 fi
 
-SN_LONG=$("$PWD"rexgen serial | tr -d '\0' | cut -d " " -f 2 | tr -d 'number' | tr -d '[:space:]')
+# Read parameters
+WATCH_DIR="$1"
+AWS_ACCESS_KEY="$2"
+AWS_SECRET_KEY="$3"
+BUCKET="$4"
+REGION="$5"
+SERIAL="$6"
 
-for i in $RXD_DIR*.rxd; do
-        FILE=$(echo $i)
-        FILE_BASE=$(basename "$FILE")
-        DATE_BASE=$(echo $FILE_BASE | rev | cut -d "_" -f 2 | rev)
+# Export AWS credentials for this session
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_KEY"
 
-        aws upload -file $FILE -key $DATE_BASE/$FILE_BASE -accesskey $ACCESS_KEY -secretkey $SECRET_KEY -region eu-central-1 -bucket $BUCKET/$SN_LONG
+while true; do
+	AWS_ACCESS_KEY=$("$PWD"/examples/read_aws_conf.sh ACCESS_KEY)
+	AWS_SECRET_KEY=$("$PWD"/examples/read_aws_conf.sh SECRET_KEY)
+	BUCKET=$("$PWD"/examples/read_aws_conf.sh BUCKET)
+	REGION=$("$PWD"/examples/read_aws_conf.sh REGION)
 
+    # Get the first file in the folder (sorted by name)
+    FILE=$(ls -1 "$WATCH_DIR"/*.rxd 2>/dev/null | head -n 1)
+
+    # If no file is found, wait and check again
+    if [ -z "$FILE" ]; then
+        sleep 5  # Wait 5 seconds before checking again
+        continue
+    fi
+
+    FILE_BASE=$(basename "$FILE")
+    DATE_BASE=$(echo "$FILE_BASE" | rev | cut -d "_" -f 2 | rev)
+
+    echo "Uploading: $FILE"
+
+    # Run the AWS upload command
+	/home/root/rexusb/aws upload -file $FILE -key $DATE_BASE/$FILE_BASE -accesskey $AWS_ACCESS_KEY -secretkey $AWS_SECRET_KEY -region $REGION -bucket $BUCKET/$SERIAL
+
+    # Check if the upload was successful
+    if [ $? -eq 0 ]; then
+        echo "Upload successful, deleting: $FILE"
+        rm -f "$FILE"
+    else
+        echo "Upload failed, keeping file: $FILE"
+    fi
+
+    sleep 2  # Small delay before checking the next file
 done
